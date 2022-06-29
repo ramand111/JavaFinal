6bf3ac5dfc10dba1f6830cc9b001c5bd4bd3ff38
ramand111 <ramandeepkaur56@loyalistcollege.com>
2022-06-29 17:50:57 -0400

    Commit From Raman

--- HelloApp/src/main/java/com/mycompany/helloapp/HelloApp.java
+++ HelloApp/src/main/java/com/mycompany/helloapp/HelloApp.java
@@ -10,7 +10,7 @@ public class HelloApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World 132");
-        System.out.println("Hraman");
+        
 
     }
 }
